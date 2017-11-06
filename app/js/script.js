e258a95cdc3575538019149a559a6addc7a26f0b
Andrey-Trzesnevskiy <rockman06@mail.ru>
2017-11-06 22:04:24 +0200

    Adaptive style added

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,13 @@
 window.onload = function(){
-    var hmbgMenu = document.querySelector('.hmbg');
-    var menu = document.querySelector('.menu');
-    hmbgMenu.addEventListener('click', function(){
-        menu.classList.toggle('is-active');
-    })
+    hmbgMenu = document.querySelector('.hmbg-logo');
+    mainMenu = document.querySelector('.main-menu');
+    hmbgNews = document.querySelector('.hmbg-news');
+    menuNews = document.querySelector('.menu-news');
+    function clickMenu (hmbgClick, menu) {
+        hmbgClick.addEventListener('click', function(){
+            menu.classList.toggle('is-active');
+        })
+    }
+    clickMenu(hmbgMenu, mainMenu);
+    clickMenu(hmbgNews, menuNews);
 }
