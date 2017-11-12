a13413f2a98ffef50e23e2dd8dd8bc39c4607c4b
Andrey-Trzesnevskiy <rockman06@mail.ru>
2017-11-12 20:24:17 +0200

    Design is fixed
    Responsive menu fixed. Grids are fixed

--- app/js/script.js
+++ app/js/script.js
@@ -3,11 +3,39 @@ window.onload = function(){
     mainMenu = document.querySelector('.main-menu');
     hmbgNews = document.querySelector('.hmbg-news');
     menuNews = document.querySelector('.menu-news');
+    input = document.querySelector('.search-container input');
     function clickMenu (hmbgClick, menu) {
-        hmbgClick.addEventListener('click', function(){
+        hmbgClick.addEventListener('click', function(event){
             menu.classList.toggle('is-active');
+            event.stopPropagation();
+        })
+    }
+    function hideMenu (menu) {
+        menu.addEventListener('mouseover', function(){
+            menu.addEventListener('click', function(){
+                menu.classList.remove('is-active');
+            })
+        });
+    };
+    function focusInput (input) {
+        input.addEventListener('click', function(event){
+            event.stopPropagation();
+        })
+    }
+    function hideMenuClickDoc (menu) {
+        menu.addEventListener('mouseout', function(e){
+            document.addEventListener('click', function(){
+                if (menu.classList.contains('is-active')) {
+                    menu.classList.remove('is-active');
+                }
+            })
         })
     }
     clickMenu(hmbgMenu, mainMenu);
     clickMenu(hmbgNews, menuNews);
+    hideMenu(mainMenu);
+    hideMenu(menuNews);
+    hideMenuClickDoc(mainMenu);
+    hideMenuClickDoc(menuNews);
+    focusInput(input);
 }
