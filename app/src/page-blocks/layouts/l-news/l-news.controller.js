6366938e071eb44c7cfdbcd8c7c37777bb7d0688
Andrey-Trzesnevskiy <rockman06@mail.ru>
2017-11-16 17:24:20 +0200

    Commit

--- app/src/page-blocks/layouts/l-news/l-news.controller.js
+++ app/src/page-blocks/layouts/l-news/l-news.controller.js
@@ -15,5 +15,24 @@ app.controller("l-news.controller", function($scope, $http) {
     }
 
     init();
-	
+	$scope.showNews = function(item, arr) {
+                newArr = [];
+                arr.forEach((el, i) => {
+                    newArr[i] = arr[i]
+                });
+                arr.splice(0, newArr.length, newArr[newArr.indexOf(item)]);
+                blocks = document.querySelectorAll('.full-width');
+                curBlock = blocks[this.$index];
+                blocks[this.$index].classList.remove('full-width');
+                button = document.querySelectorAll('.show');
+                curButton = button[this.$index];
+                button[this.$index].style.display = "none";
+           }
+           $scope.showAll = function (item, arr) {
+                curBlock.classList.add('full-width');
+                curButton.style.display = "block";
+                newArr.forEach((el, i) =>{
+                    arr[i] = newArr[i]
+                });
+            }
 });
