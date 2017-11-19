adaf865a6fd67b0f16bb2d8aabf363b8f6d9c640
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-11-20 00:58:32 +0200

    updated pages News and Bands

--- app/src/page-blocks/layouts/l-bands/l-bands.controller.js
+++ app/src/page-blocks/layouts/l-bands/l-bands.controller.js
@@ -1,6 +1,9 @@
-app.controller("l-bands.controller", function($scope, $http){
+app.controller("l-bands.controller", function($scope, $http) {
+    
     $scope.dataBands = {};
+    
     function init() {
+        
         $http({
             method: 'GET',
             url: 'http://localhost:5000/bands'
@@ -10,9 +13,36 @@ app.controller("l-bands.controller", function($scope, $http){
                 $scope.dataBands = response.data.bandsAll;
                 for(let i=0; i<$scope.dataBands.length; i++){
                     console.log($scope.dataBands[i].band, 'состав: ', $scope.dataBands[i].cast);
+                     
+                    displayLastBand($scope.dataBands);
                 }
             });
+           
     }
+    
     init();
+      
+      
+      
+	$scope.$on("bandDataEvent2", function(event, data){console.log(data);
+        
+		$scope.currentBand = data;
+        console.log($scope.currentBand);
+        
+        $scope.currentMembers = data.band.cast;
+        console.log($scope.currentMembers);
+	});
+    
+    
+    
+    function displayLastBand(data) {
+         
+        $scope.currentBand = [];
+        $scope.currentBand.push(data[0]);
+        console.log( $scope.currentBand );
+                        
+        $scope.currentMembers = data[0].cast;
+        console.log( $scope.currentMembers );
+    }
 
 });
