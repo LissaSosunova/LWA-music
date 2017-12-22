63be163989a071f487ab11db958c2a22796311b4
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-12-22 15:20:47 +0200

    created info-button

--- app/src/common-blocks/root.controller.js
+++ app/src/common-blocks/root.controller.js
@@ -1,15 +1,38 @@
 app.controller("root.controller", function($scope, $http) {
-     $scope.dataMenu = {};
-
-    (function init() {
-        
+    
+    $scope.dataMenu = {};
+    
+    $scope.dataPoint = {
+        list: [],
+        actions: [
+            function(){
+                console.log(111);
+            },
+            function(){
+                console.log(222);
+            },
+            function(){
+                console.log(333);
+            }  
+        ]
+    };
+    
         $http({
             method: 'GET',
             url: 'http://localhost:5000/menu'
         })
-            .then(function success(response) {
-                $scope.dataMenu = response.data.dataItems;
-            });
-    })();
+        .then(function success(response) {
+            console.log('ответ сервера по menu');
+            $scope.dataMenu = response.data.dataItems;
+        });
+            
+        $http({
+            method: 'GET',
+            url: 'http://localhost:5000/point__block'
+        })
+        .then(function success(response) {
+            console.log('ответ сервера по point__block');
+            $scope.dataPoint.list = response.data.dataItems;
+        });
     
 });
