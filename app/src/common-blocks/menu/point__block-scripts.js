a77ab32616250bacf722aaf369d7f30a398bd6ad
Alexander Tsimbalyuk <a.tsimbalyuk1@gmail.com>
2017-12-24 05:55:50 +0200

    fixed info-button 2.0

--- app/src/common-blocks/menu/point__block-scripts.js
+++ app/src/common-blocks/menu/point__block-scripts.js
@@ -4,14 +4,28 @@ window.onload = function(){
     var dropdown = document.querySelector(".project-info__data");
     var item = document.querySelectorAll(".project-info__data-item");
     
-    dotBtn.onclick = function() {
+    dotBtn.addEventListener("click", function(e){
+        showMenu();
+        clickBehindMenu(dropdown, function (){
+            hideMenu();
+        });
+    });
+    for (var i = 0; i < item.length; i++){
+        item[i].addEventListener("click", hideMenu);
+    }
+    function showMenu(){
         dropdown.style.display = "block";
     }
-    for (var i = 0; i < item.length; i++){
-        item[i].addEventListener('click', hideMenu);
-    }
-    function hideMenu() {
+    function hideMenu(){
         dropdown.style.display = "none";
     }
- 
+    var clickBehindMenu = function(elem, func) {
+        var behindMenu = function (e) {
+            if (e.target !== elem) {
+                document.removeEventListener("click", behindMenu, true);
+                func();
+            }
+        }
+        document.addEventListener("click", behindMenu, true);
+    }
 };
